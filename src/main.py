76416b9f2ba104fdcda5c1b0d278b215add0e302
Treeve White <treevew@gmail.com>
2024-02-05 19:39:24 +0000

    Imporve logging and output file structure

--- src/main.py
+++ src/main.py
@@ -1,10 +1,12 @@
+import sys
 import tensorflow as tf
 from keras.preprocessing import image_dataset_from_directory
 from datetime import datetime
+import os
 
 from cnn_models import dense_net
 
-from config import IMAGE_SIZE, LOG_PATH, MODEL_SAVE_PATH, DATASET_PATH
+from config import IMAGE_SIZE, MODEL_SAVE_PATH, DATASET_PATH
 
 STRATEGY = tf.distribute.get_strategy()
 AUTOTUNE = tf.data.AUTOTUNE
@@ -16,50 +18,56 @@ MODEL_NAME = "DENSENET"
 
 
 if __name__ == "__main__":
+    base_dir = f"{MODEL_SAVE_PATH}/{MODEL_NAME}-{datetime.now().strftime('%Y-%m-%d-%H:%M')}"
+    os.makedirs(base_dir, exist_ok=True)
 
-    print("REPLICAS: ", STRATEGY.num_replicas_in_sync)
+    with open(f"{base_dir}/log.log", "w") as sys.stdout:
 
-    train_ds = image_dataset_from_directory(
-        DATASET_PATH,
-        labels="inferred",
-        label_mode="categorical",
-        image_size=IMAGE_SIZE,
-        color_mode="grayscale",
-        validation_split=0.2,
-        subset="training",
-        seed=1337
-    ).prefetch(buffer_size=AUTOTUNE)
+        print("REPLICAS: ", STRATEGY.num_replicas_in_sync)
 
-    validation_ds = image_dataset_from_directory(
-        DATASET_PATH,
-        labels="inferred",
-        label_mode="categorical",
-        image_size=IMAGE_SIZE,
-        color_mode="grayscale",
-        validation_split=0.2,
-        subset="validation",
-        seed=1337
-    ).prefetch(buffer_size=AUTOTUNE)
+        train_ds = image_dataset_from_directory(
+            DATASET_PATH,
+            labels="inferred",
+            label_mode="categorical",
+            image_size=IMAGE_SIZE,
+            color_mode="grayscale",
+            validation_split=0.2,
+            subset="training",
+            seed=1337
+        ).prefetch(buffer_size=AUTOTUNE)
 
-    with STRATEGY.scope():
-        model = dense_net()
+        validation_ds = image_dataset_from_directory(
+            DATASET_PATH,
+            labels="inferred",
+            label_mode="categorical",
+            image_size=IMAGE_SIZE,
+            color_mode="grayscale",
+            validation_split=0.2,
+            subset="validation",
+            seed=1337
+        ).prefetch(buffer_size=AUTOTUNE)
 
-    model.summary()
+        with STRATEGY.scope():
+            model = dense_net()
 
-    model.compile(optimizer='adam',
-                  loss='categorical_crossentropy',
-                  metrics=['accuracy'])
+        model.summary()
 
-    # Defining Callbacks
-    save_best = tf.keras.callbacks.ModelCheckpoint(
-        filepath=f"{MODEL_SAVE_PATH}/{MODEL_NAME}-{datetime.now()}.h5", monitor='val_loss', save_best_only=True)
-    reduce_lr = tf.keras.callbacks.ReduceLROnPlateau(
-        patience=10, monitor='val_accuracy', factor=0.6, min_lr=0.0000001)
+        model.compile(optimizer='adam',
+                    loss='categorical_crossentropy',
+                    metrics=['accuracy'])
 
-    history = model.fit(
-        train_ds,
-        validation_data=validation_ds,
-        epochs=EPOCHS,
-        callbacks=[save_best, reduce_lr]
-    )
-    
+        
+
+        # Defining Callbacks
+        save_best = tf.keras.callbacks.ModelCheckpoint(
+            filepath=f"{base_dir}/model.h5", monitor='val_loss', save_best_only=True)
+        reduce_lr = tf.keras.callbacks.ReduceLROnPlateau(
+            patience=10, monitor='val_accuracy', factor=0.6, min_lr=0.0000001)
+
+        history = model.fit(
+            train_ds,
+            validation_data=validation_ds,
+            epochs=EPOCHS,
+            callbacks=[save_best, reduce_lr]
+        )
+        
