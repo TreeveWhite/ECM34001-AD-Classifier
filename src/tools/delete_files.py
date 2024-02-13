3e7c56a00f492ca62e2341bb5d23a00682b22fa7
Treeve White <treevew@gmail.com>
2024-02-13 22:07:01 +0000

    Add tool to x amount of delete files

---
+++ src/tools/delete_files.py
@@ -0,0 +1,19 @@
+import os
+
+
+def delete_files(directory):
+    files = os.listdir(directory)
+    files.sort()  # Sort files alphabetically
+    count = 0
+
+    for file in files:
+        count += 1
+        if count % 5 != 1:  # Keep every 5th file, delete others
+            file_path = os.path.join(directory, file)
+            os.remove(file_path)
+            print(f"Deleted: {file_path}")
+
+
+if __name__ == "__main__":
+    directory_path = "/home/white/uni_workspace/ecm3401-dissertation/data/ADNI_SLICE_TRAINING_DATA/BAD"
+    delete_files(directory_path)
