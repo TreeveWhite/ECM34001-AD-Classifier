1ec9075ae37754e723f56f1a84e48876b9f38ce4
Treeve White <treevew@gmail.com>
2024-02-23 10:00:58 +0000

    Release V2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="ad_classifier",
-    version="1.2.0",
+    version="2.0.0",
     author="Treeve White",
     author_email="treevew@gmail.com",
     description="AD Model",
