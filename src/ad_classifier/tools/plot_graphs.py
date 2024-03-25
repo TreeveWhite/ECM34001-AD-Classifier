52f1119a4ebe8a3946c100f4c77d94ee7d7549e2
Treeve White <treevew@gmail.com>
2024-03-25 14:49:43 +0000

    Create graphs from metrics

--- src/ad_classifier/tools/plot_graphs.py
+++ src/ad_classifier/tools/plot_graphs.py
@@ -1,10 +1,73 @@
+from matplotlib import rc
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 from matplotlib_inline.backend_inline import set_matplotlib_formats
+import matplotlib as mpl
+
+plt.style.use('seaborn-v0_8-darkgrid')
+font = {'size': 16}
+mpl.rc('font', **font)
+rc('font', **{'family': 'sans-serif', 'sans-serif': ['Helvetica']})
 
 set_matplotlib_formats('retina', quality=1000)
+
+
+def plot_survey_bars():
+    data = {
+        'Question': [
+            'How valuable do you think\nCogniCheck and similar AI\nsystems could be in Healthcare?',
+            'How successfully do the\ngenerated visualisations\nexplain the decisions made\nby the AIbehind a diagnosis?',
+            'How easy is the system\nto use and navigate?',
+            """As a whole, how do\n you rate the system?"""
+        ],
+        'Average Response (1-10)': [8, 7.5, 9, 9.5]
+    }
+
+    # data = {
+    #     'Question': [
+    #         'Ease of use',
+    #         'Clear diagnoses and results',
+    #         'Understandable AI diagnoses',
+    #         'Intuitive system',
+    #         'Customisability and user profiles',
+    #         'System Compatibility with\ntablets & mobile devices',
+    #         'Collaborative features'
+    #     ],
+    #     'Average Importance (1-10)': [8.75, 9.5, 8.5, 7, 4.5, 7, 7]
+    # }
+
+    df = pd.DataFrame(data)
+   # Extract necessary columns
+    questions = df['Question']
+    ratings = df['Average Response (1-10)']
+
+    # Plotting
+    fig, ax = plt.subplots(figsize=(10, 6))
+
+    # Define colors
+    colors = ['#1f77b4', '#4d88ff', '#99bbff', "#ff7f0e"]
+
+    ax.barh(questions, ratings, color=colors)  # Use barh for horizontal bars
+
+    ax.set_xlabel("Average Response (1-10)")  # Change to xlabel
+    ax.set_xlim(xmin=0, xmax=10)  # Change to xlim
+    ax.grid(axis='x', linestyle='--', alpha=0.7)  # Change to x-axis grid
+
+    # Add labels
+    for bar in ax.patches:
+        ax.annotate("{:.1f}".format(bar.get_width()),
+                    (bar.get_width(), bar.get_y() + bar.get_height() / 2),
+                    va='center', ha='left', color='black')  # Adjust position and alignment
+
+    # Add title
+    ax.set_title(
+        'Results of End User Testing Survey', fontsize=20)
+
+    # Adjust layout
+    fig.tight_layout()
+    fig.savefig("plotter.png", bbox_inches='tight', dpi=300)
 
 
 def plot_bar(df):
@@ -15,90 +78,83 @@ def plot_bar(df):
     sensitivity = df['Sensitivity']
     f1_score = df['F1 Score']
 
+    accuracy = list(map(lambda x: x * 100, accuracy))
+
     # Plotting
-    fig, ax = plt.subplots(figsize=(10, 5))
+    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(15, 5))
 
     # Define colors
-    colors = ['#1f77b4', '#4d88ff', '#99bbff', '#cceeff']
-    metrics = ['Accuracy', 'Precision', 'Sensitivity', 'F1 Score']
-    data = [accuracy, precision, sensitivity, f1_score]
+    colors = ['#1f77b4', '#4d88ff', '#99bbff', '#cceeff', "#ff7f0e"]
 
-    # Bar width
-    bar_width = 0.2
-    # Positions of bars on x-axis
-    r = range(len(model_names))
+    ax1.bar(model_names, accuracy, color=colors)
 
-    for i, metric in enumerate(metrics):
-        bars = ax.bar([x + i * bar_width for x in r], data[i],
-                      width=bar_width, label=metric, color=colors[i])
-
-    ax.set_ylabel("%")
-    ax.set_xticks([r + 1.5 * bar_width for r in range(len(model_names))])
-    ax.set_xticklabels(model_names)
-    ax.grid(axis='y', linestyle='--', alpha=0.7)
-    ax.spines['top'].set_visible(False)
-    ax.spines['right'].set_visible(False)
-    ax.spines['bottom'].set_color('#DDDDDD')
-    ax.spines['left'].set_color('#DDDDDD')
-    ax.set_axisbelow(True)
-    ax.yaxis.grid(True, color='#EEEEEE')
-    ax.xaxis.grid(False)
+    ax1.set_ylabel("%")
+    ax1.set_ylim(ymin=0, ymax=100)
+    ax1.grid(axis='y', linestyle='--', alpha=0.7)
 
     # Add labels
-    for bar in ax.patches:
-        ax.annotate("{:.1f}".format(bar.get_height()*100),
-                    (bar.get_x() + bar.get_width() / 2, bar.get_height()),
-                    ha='center', va='bottom', color='black')
+    for bar in ax1.patches:
+        ax1.annotate("{:.3f}".format(bar.get_height()),
+                     (bar.get_x() + bar.get_width() / 2, bar.get_height()),
+                     ha='center', va='bottom', color='black')
 
     # Add legend
-    ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+    ax1.legend(loc='center left', bbox_to_anchor=(1, 0.5))
 
     # Add title
-    plt.title('Evaluative Performance Metrics by Model', fontsize=16)
+    ax1.set_title(
+        'Accuracy of Experimental Models during Testing', fontsize=16)
+
+    # Create a table
+    table_data = {
+        'Model Name': model_names,
+        'Sensitivity': sensitivity,
+        'Precision': precision,
+        'F1 Score': f1_score
+    }
+    ax2.axis('off')  # Hide axis
+    print(table_data.values())
+    ax2.table(cellText=list(table_data.values()),
+              colLabels=list(table_data.keys()), loc='center')
 
     # Adjust layout
     fig.tight_layout()
-    plt.savefig("./var_graph.png")
+    fig.savefig("plotter.png", bbox_inches='tight', dpi=300)
 
 
 def plot_line(df):
     # Extract necessary columns
-    epochs = df['Epoch']
-    validation_loss1 = df['SDG Optimisation Validation Loss']
-    validation_loss2 = df['No Optimisation Validation Loss']
-    validation_loss3 = df['ADAM']
+    epochs = df['epoch']
+
+    results1 = accuracy = list(map(lambda x: x * 100, df['accuracy']))
+    results2 = df['loss']
+    results3 = accuracy = list(map(lambda x: x * 100, df['val_accuracy']))
+    results4 = df['val_loss']
 
     # Plotting
     fig, ax = plt.subplots(figsize=(8, 5))
 
-    # Plot lines for loss and validation loss
-    ax.plot(epochs, validation_loss1, label='SDG Optimisation Validation Loss',
-            marker='o', color='#ff7f0e')
-    ax.plot(epochs, validation_loss2, label='No Optimisation Validation Loss',
-            marker='o', color='#99bbff')
-    ax.plot(epochs, validation_loss3, label='ADAM Optimisation Validation Loss',
+    ax.plot(epochs, results1, label='VGGNet19 Training Accuracy',
             marker='o', color='#1f77b4')
+    ax.plot(epochs, results3, label='VGGNet19 Validation Accuracu',
+            marker='o', color='#ff7f0e')
 
-    ax.set_ylabel("Loss")
+    ax.set_ylabel("Accuracy")
+    # ax.set_ylim(ymin=0.8, ymax=1)
+    # ax.set_xlim(xmin=1, xmax=10)
     ax.set_xlabel("Epoch")
-    ax.grid(axis='y', linestyle='--', alpha=0.7)
-    ax.spines['top'].set_visible(False)
-    ax.spines['right'].set_visible(False)
-    ax.spines['bottom'].set_color('#DDDDDD')
-    ax.spines['left'].set_color('#DDDDDD')
-    ax.set_axisbelow(True)
-    ax.yaxis.grid(True, color='#EEEEEE')
-    ax.xaxis.grid(False)
 
     # Add legend
-    ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+    ax.legend(loc="upper right")
 
     # Add title
-    plt.title('Loss of Models using SDG and No Optimisers', fontsize=16)
+    plt.title(
+        'Training and Validation Accuracy of VGGNet19', fontsize=16)
 
     # Adjust layout
     fig.tight_layout()
-    plt.savefig("./line_graph.png")
+    fig.savefig("plotter.png", bbox_inches='tight', dpi=300)
+    plt.show()
 
 
 def plot_conf():
@@ -133,41 +189,19 @@ def plot_full_bars():
         'F1 Score': [0.92358853, 0.919861441, 0.918043505, 0.982192544, 0.75562, 0.75693, 0.74918, 0.74332, 0.91648, 0.89682, 0.94161, 0.90475]
     }
 
+    # Convert data to DataFrame
     df = pd.DataFrame(data)
 
-    # Plotting
-    fig, ax = plt.subplots(figsize=(14, 8))
+    # Pivot the DataFrame to create a matrix of accuracies
+    heatmap_data = df.pivot_table(values='F1 Score', index='Optimizer', columns=[
+        'Pooling Method', 'Loss Function'])
 
-    # Define colors
-    colors = ['#1f77b4', '#ff7f0e', '#2ca02c', '#d62728']
-    metrics = ['Accuracy', 'Precision', 'Sensitivity', 'F1 Score']
+    # Plot the heatmap
+    plt.figure()
+    sns.heatmap(heatmap_data, cmap='RdYlGn',
+                annot=True, fmt=".2f", linewidths=.5)
 
-    # Bar width
-    bar_width = 0.2
-
-    for i, metric in enumerate(metrics):
-        for idx, row in df.iterrows():
-            x = i * bar_width + idx + 0.5
-            ax.bar(x, row[metric], width=bar_width,
-                   label=row['Optimizer'], color=colors[i])
-
-    # Add labels and title
-    ax.set_ylabel("%")
-    ax.set_xticks([i + 0.5 for i in range(len(df) * len(metrics))])
-    ax.set_xticklabels([f"{row['Pooling Method']} ({row['Loss Function']})" for idx,
-                        row in df.iterrows()], rotation=45, ha='right')
-    ax.grid(axis='y', linestyle='--', alpha=0.7)
-    ax.spines['top'].set_visible(False)
-    ax.spines['right'].set_visible(False)
-    ax.spines['bottom'].set_color('#DDDDDD')
-    ax.spines['left'].set_color('#DDDDDD')
-    ax.set_axisbelow(True)
-    ax.yaxis.grid(True, color='#EEEEEE')
-    ax.xaxis.grid(False)
-    ax.legend(loc='upper left', bbox_to_anchor=(1, 1))
-    plt.title(
-        'Evaluative Performance Metrics by Optimizer, Loss Function, and Pooling Method', fontsize=16)
-
-    # Adjust layout
-    fig.tight_layout()
-    plt.savefig("./graph.png")
+    plt.title('F1 Score Heatmap of Fine Tuning Experiments')
+    plt.xlabel('Pooling Method - Loss Function')
+    plt.ylabel('Optimiser')
+    plt.savefig("full.png", bbox_inches='tight', dpi=300)
